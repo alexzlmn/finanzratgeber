1dc9f7a1814fa7c27aa6a27ba7b849180c206b1c
Alex <alexander_robert.zellmann@smail.th-koeln.de>
2024-01-18 19:33:28 +0100

    first component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import UserFinances from './components/UserFinances.vue'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const appEinnahmen = createApp(UserFinances)
+appEinnahmen.mount('#userfinances')
